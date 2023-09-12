50c0d69c2ff8b71a0b3f9810d9c2a456282d4c69
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-09-12 16:56:13 +0700

    add Liked button & page with IndexedDB

--- src/scripts/views/pages/upcoming.js
+++ src/scripts/views/pages/upcoming.js
@@ -7,6 +7,7 @@ const Upcoming = {
       <div class="content">
         <h2 class="content__heading">Upcoming in Cinema</h2>
         <div id="movies" class="movies">
+
         </div>
       </div>
     `;
@@ -15,7 +16,6 @@ const Upcoming = {
   async afterRender() {
     const movies = await TheMovieDbSource.upcomingMovies();
     const moviesContainer = document.querySelector('#movies');
-
     movies.forEach((movie) => {
       moviesContainer.innerHTML += createMovieItemTemplate(movie);
     });
